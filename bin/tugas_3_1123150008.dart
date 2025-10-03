5c5a9646512d8e0d417d6715c5440e960dd2e41f
dwiilhammaulana <1123150008@global.ac.id>
2025-10-03 21:40:24 +0700

    first commit

---
+++ bin/tugas_3_1123150008.dart
@@ -0,0 +1,5 @@
+import 'package:tugas_3_1123150008/tugas_3_1123150008.dart' as tugas_3_1123150008;
+
+void main(List<String> arguments) {
+  print('Hello world: ${tugas_3_1123150008.calculate()}!');
+}
